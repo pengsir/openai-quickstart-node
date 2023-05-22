031dc6936a3339392a4ab7046cf33239c4df833b
hui peng <hpeng@tripactions.com>
2023-05-22 16:12:59 -0700

    support UTF-8

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -34,7 +34,7 @@ export default async function (req, res) {
       temperature: 0,
       max_tokens: 4000
     });
-    console.log('completion', completion.data.choices[0].text);
+    console.log('completion', completion.data);
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
