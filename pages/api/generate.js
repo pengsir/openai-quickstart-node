d3dade0f8384f51fafb356490686205cbd371a6a
hui peng <hpeng@tripactions.com>
2023-05-22 21:11:51 -0700

    support UTF-8

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -32,7 +32,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: prompt,
       temperature: 0,
-      max_tokens: 256,
+      max_tokens: 4000,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0
